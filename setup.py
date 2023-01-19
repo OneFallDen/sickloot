3d997c93f99bf7adda8bd81308bb6567707316aa
OneFallDen <onefallden@gmail.com>
2023-01-19 18:13:11 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="sickloot",
+    version="1.0.0",
+    url="https://github.com/OneFallDen/sickloot",
+    packages=find_packages()
+)
