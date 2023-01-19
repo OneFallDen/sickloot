28030d5336b4f8e7791a4c1b864cc4943711ac5d
OneFallDen <onefallden@gmail.com>
2023-01-19 17:47:45 +0300

    added engine for db

---
+++ sql/database.py
@@ -0,0 +1,17 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE = os.environ['DATABASE']
+DB_USER = os.environ['DB_USER']
+DB_PASSWORD = os.environ['DB_PASSWORD']
+DB_NAME = os.environ['DB_NAME']
+DB_HOST = os.environ['DB_HOST']
+
+POSTGRESQL_CONFIG = f"{DATABASE}+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+
+
+engine = create_engine(POSTGRESQL_CONFIG, echo=True, future=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
