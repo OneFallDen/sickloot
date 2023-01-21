770965e786dfcfe4dd1a12f6150b51bf42b47f83
OneFallDen <onefallden@gmail.com>
2023-01-22 01:16:17 +0300

    included case router

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import user_router, balance_router
+from routers import user_router, balance_router, case_router
 
 
 app = FastAPI()
@@ -7,3 +7,4 @@ app = FastAPI()
 
 app.include_router(user_router.router)
 app.include_router(balance_router.router)
+app.include_router(case_router.router)
