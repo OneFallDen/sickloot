cb0f7257914dcf32391f817a3c52b92536ae8ddd
OneFallDen <onefallden@gmail.com>
2023-01-21 18:52:00 +0300

    added balance windrow

--- controllers/balance_controller.py
+++ controllers/balance_controller.py
@@ -6,6 +6,18 @@ from sql.crud import update_balance
 
 def balance_rep(rep_balance: int, user_id: int, db: Session):
     if not rep_balance > 0:
-        raise HTTPException(status_code=204, detail='Invalid sum for replenishment')
+        raise HTTPException(status_code=400, detail='Invalid sum for replenishment')
     update_balance(rep_balance, user_id, db)
     return HTTPException(status_code=201, detail='Balance replenished successfully')
+
+
+def balance_wind(win_balance: int, user_id: int, db: Session):
+    if win_balance == 0:
+        raise HTTPException(status_code=400, detail='Invalid sum for windrow')
+    if win_balance > 0:
+        win_balance = win_balance * (-1)
+    if update_balance(win_balance, user_id, db):
+        return HTTPException(status_code=201, detail='Balance windrow successfully')
+    else:
+        return HTTPException(status_code=400, detail='Invalid sum for windrow')
+
--- routers/balance_router.py
+++ routers/balance_router.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from models.schemas import User
 from controllers.user_controller import get_current_user
-from controllers.balance_controller import balance_rep
+from controllers.balance_controller import balance_rep, balance_wind
 from sql.db import get_db
 
 
@@ -13,3 +13,8 @@ router = APIRouter()
 @router.post('/balance/rep', tags=['balance'])
 async def replenish_balance(rep_balance: int, user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     return balance_rep(rep_balance, user.id, db)
+
+
+@router.post('/balance/wind', tags=['balance'])
+async def windrow_balance(win_balance: int, user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    return balance_wind(win_balance, user.id, db)
