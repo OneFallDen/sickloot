8ea8f63550f9c33ea2a46d761d6247f22b2ffe96
OneFallDen <onefallden@gmail.com>
2023-01-21 20:42:08 +0300

    small fixes

--- routers/user_router.py
+++ routers/user_router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
-from models.schemas import Token, UserReg, UserAuth
+from models.schemas import UserReg
 from controllers.user_controller import signup, signin
 from sql.db import get_db
 
