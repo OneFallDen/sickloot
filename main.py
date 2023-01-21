473fc04ca5efcf0857c4f9a72ce072db4a8933a7
OneFallDen <onefallden@gmail.com>
2023-01-21 18:28:28 +0300

    included balance router

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from routers import user_router
+from routers import user_router, balance_router
 
 
 app = FastAPI()
 
 
 app.include_router(user_router.router)
+app.include_router(balance_router.router)
