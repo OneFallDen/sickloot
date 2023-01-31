8b098c45e8d527b5088479b7421582d996309f1e
OneFallDen <onefallden@gmail.com>
2023-01-31 11:15:48 +0300

    added selling items from inventory

--- controllers/inventory_controller.py
+++ controllers/inventory_controller.py
@@ -1,8 +1,15 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException
 
-from sql.crud import get_inventory_items
+from sql.crud import get_inventory_items, delete_item_from_inventory, get_item_by_id, update_balance
 
 
 def get_inventory(user_id: int, db: Session):
     return get_inventory_items(user_id, db)
+
+
+def sell_item_from_user_inventory(item_id: int, user_id: int, db: Session):
+    delete_item_from_inventory(item_id, user_id, db)
+    item = get_item_by_id(item_id, db)
+    update_balance(item.price, user_id, db)
+    return HTTPException(status_code=201, detail='Item deleted successfully')
--- routers/inventory_router.py
+++ routers/inventory_router.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from sql.db import get_db
 from models.schemas import User
 from controllers.user_controller import get_current_user
-from controllers.inventory_controller import get_inventory
+from controllers.inventory_controller import get_inventory, sell_item_from_user_inventory
 
 
 router = routing.APIRouter()
@@ -13,3 +13,8 @@ router = routing.APIRouter()
 @router.get('/inventory/user', tags=['inventory'])
 async def get_user_inventory(user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     return get_inventory(user.id, db)
+
+
+@router.post('/inventory/sell', tags=['inventory'])
+async def sell_item_from_inventory(item_id: int, user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    return sell_item_from_user_inventory(item_id, user.id, db)
